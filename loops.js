f96178ad838f4187f1b4342614e103b0d3c28019
Melike KILIC <melike180400@gmail.com>
2019-09-13 22:10:54 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -19,12 +19,12 @@ return 'done';
 
 function doWhileLoop(num) {
   var i = 0;
- 
+
 function incrementVariable() {
   i = i + 1;
   return i;
 }
- 
+
   do {
     console.log("I run once regardless.")
   } while (incrementVariable() < 5);
