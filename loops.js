7198297e074343e2106f272cbfcd35b62e15f9f3
Melike KILIC <melike180400@gmail.com>
2019-09-13 21:56:51 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -17,13 +17,13 @@ function whileLoop(number){
 return 'done';
 }
 
-function maybeTrue() {
-  return Math.random() >= 0.5
-}
-
 function doWhileLoop(array) {
- do {
-   array = array.slice(1)
- } while (array.length > 0 && maybeTrue())
- return array
- }
+  function maybeTrue() {
+    return Math.random() >= 0.5
+  }
+
+  do {
+    array.pop();
+  } while (array.length > -1 && maybeTrue());
+    return array;
+}
