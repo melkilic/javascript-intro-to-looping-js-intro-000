993e50d18e3dbae2aac6c9b91c6842e7aedd4f92
Melike KILIC <melike180400@gmail.com>
2019-09-13 21:53:27 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -23,7 +23,7 @@ function maybeTrue() {
 
  function doWhileLoop(num){
 do {
-  array = array.slice(1)
- } while (array.length > 0 && maybeTrue())
- return array
+  num = num.slice(1)
+} while (num.length > 0 && maybeTrue())
+ return num
 }
